3017e4dd3b5c376597c5016dfd355dc562e575f5
Fareez <fareez1234@gmail.com>
2020-03-19 14:02:10 +1100

    Attend-1: Fix Prop-Type issue

--- sports-anywhere/src/components/button/index.js
+++ sports-anywhere/src/components/button/index.js
@@ -6,8 +6,13 @@ import './button.scss';
 const Button = ({variation = "document", children}) => <button className={`btn btn--${variation}`}>{children}</button>;
 
 Button.propTypes = {
-    variation: PropTypes.string.isRequired,
+    variation: PropTypes.string,
     children: PropTypes.node.isRequired,
+}
+
+
+Button.defaultProps = {
+    variation: "document",
 }
 
 export default React.memo(Button);
