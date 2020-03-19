e02f364de76b66dcc111a46fb3182ef0834ac854
Fareez <fareez1234@gmail.com>
2020-03-19 14:49:25 +1100

    Attend-1: Adding production code

---
+++ sports-anywhere/production/precache-manifest.d88e0f7a486aeb2d8b03936f57db3508.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc9522be5d01845413e692959424086",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bd2341538dc6c753762",
+    "url": "/static/css/main.6ef3cb63.chunk.css"
+  },
+  {
+    "revision": "b5d666a5d572968b7bcc",
+    "url": "/static/js/2.f7db12a9.chunk.js"
+  },
+  {
+    "revision": "37b5fb9fa73e0688add42527cf5813eb",
+    "url": "/static/js/2.f7db12a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd2341538dc6c753762",
+    "url": "/static/js/main.d7d433bb.chunk.js"
+  },
+  {
+    "revision": "87e475f36dfc17f3decf",
+    "url": "/static/js/runtime-main.93e686ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
